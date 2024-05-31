c382e500758a63f6adae7ae0899ec7027331c418
Suigetsuu <140938786+Suigetsu00@users.noreply.github.com>
2024-05-31 22:47:12 +0800

    Add files via upload

---
+++ Basic order menu.py
@@ -0,0 +1,25 @@
+fruits = ["Apple", "Orange", "Mango"]
+vegetables = ["Tomato", "Celery", "Broccoli"]
+meat = ["Chicken", "Pork", "Beef"]
+
+print("Type 1 for fruits")
+print("Type 2 for vegetables")
+print("Type 3 for meat")
+
+while True:
+    menu = int(input("Type the menu number: "))
+    menu = int(menu)
+    if menu == 1:
+        print(fruits)
+        break
+    elif menu == 2:
+        print(vegetables)
+        break
+    elif menu == 3:
+        print(meat)
+        break
+    elif menu == 0:
+        print("Exiting...")
+        break
+    else:
+        print("Invalid Number, not on the menu!")
